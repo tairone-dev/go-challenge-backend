3ef0537d12081bf68661d24e5713054c2b10dd55
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2020-05-26 21:02:31 -0300

    Challenge Implementation

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((accumulator, transaction) => {
+      if (transaction.type !== 'income') {
+        return accumulator;
+      }
+
+      return accumulator + transaction.value;
+    }, 0);
+
+    const outcome = this.transactions.reduce((accumulator, transaction) => {
+      if (transaction.type !== 'outcome') {
+        return accumulator;
+      }
+
+      return accumulator + transaction.value;
+    }, 0);
+
+    const total = income - outcome;
+
+    const balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
